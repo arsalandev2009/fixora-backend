f919a028f3fa3fee254c6b3517ba119c983475f9
Arsalan <arsalan252009@gmail.com>
2026-09-26 21:05:42 +0500

    check

--- src/db/ConnectDB.js
+++ src/db/ConnectDB.js
@@ -1,14 +1,9 @@
 import mongoose from 'mongoose'
-let isConnected = false
+// let isConnected = false
 export async function ConnectDB(){
-    try{
-        mongoose.connect(process.env.MONGODB_URL,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-    }catch(error){
-        console.error('Error connecting to db:'.error)
-    }
+
+        mongoose.connect(process.env.MONGODB_URL)
+
     
-    // .then(()=>{console.log('MongoDB Connected')}).then((err)=>{console.log(err)})
+    .then(()=>{console.log('MongoDB Connected')}).then((err)=>{console.log(err)})
 }
